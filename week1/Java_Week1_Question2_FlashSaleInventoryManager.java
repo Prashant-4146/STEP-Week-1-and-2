199af7def43865181419d595512da9a34d8cec84
prashant choudhary <pc1412@srmist.edu.in>
2026-03-12 08:35:48 +0530

    Create Java_Week1_Question2_FlashSaleInventoryManager.java

---
+++ week1/Java_Week1_Question2_FlashSaleInventoryManager.java
@@ -0,0 +1,7 @@
+// Java_Week1_Question2_FlashSaleInventoryManager
+
+public class FlashSaleInventoryManager {
+    public static void main(String[] args) {
+        // TODO: Implement the functionality
+    }
+}
